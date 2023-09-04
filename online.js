ceeb96d7ca63c20beec392ed2ace58550e361157
IamClodate <clodatemnisi@gmail.com>
2023-09-04 15:17:07 +0200

    added screen responsiveness

--- online.js
+++ online.js
@@ -1,34 +1,38 @@
-var nameError =document.getElementById('name-error');
-var emailError =document.getElementById('email-error');
-var messageError =document.getElementById('message-erro');
-var submitError =document.getElementById('submit-error');
+
+var nameError = document.getElementById('name-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
 
 
 function validateName(){
     var name = document.getElementById('contact-name').value;
     
-    if(name.length == 0){
-        nameError.innerHTML = 'Name is required'
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
         return false;
     }
-    if(!name.match(/^[A-Za-z]*\s{1}{A-Za-z}*&/)){
+    if (!name.match(/^[A-Za-z]+\s[A-Za-z]+$/)) {
         nameError.innerHTML = 'Please write Full Name';
         return false;
     }
+    
     nameError.innerHTML = '<i class="fa-solid fa-check"></i>';
     return true;
 }
+
 function validateEmail(){
     var email = document.getElementById('contact-email').value;
 
-    if(email.length == 0){
+    if (email.length == 0) {
         emailError.innerHTML = 'Email is required'
         return false;
     }
-    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-Z]*[\.][a-z]{2,4}$/)){
-        emailError.innerHTML = 'Email Invalid'
+    if (!email.match(/^[A-Za-z._-]+@[A-Za-z]+\.[a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Email Invalid';
         return false;
     }
+    
     emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
     return true;
 }
@@ -38,7 +42,7 @@ function validateMessage() {
     var required = 30;
     var left = required - message.length;
 
-    if(left>0){
+    if (left > 0) {
         messageError.innerHTML = left + ' more characters required';
         return false;
     }
@@ -47,28 +51,60 @@ function validateMessage() {
     return true;
 }
 
-// function validateForm(){
-
-//     if(!validateName() || !validateEmail() || !validateMessage()){
-//         submitError.style.display = 'block';
-//         submitError.innerHTML = 'Please fix error to submit';
-//         setTimeout(function(){submitError.style.display = 'none';}, 3000);
-//         return false;
-//     }
-// }
-
-function validateForm() {
-    var isNameValid = validateName();
-    var isEmailValid = validateEmail();
-    var isMessageValid = validateMessage();
-    
-    if (!isNameValid || !isEmailValid || !isMessageValid) {
+function validateForm(){
+    if(!validateName() || !validateEmail() || !validateMessage()){
         submitError.style.display = 'block';
-        submitError.innerHTML = 'Please fix errors to submit';
-        setTimeout(function () {
-            submitError.style.display = 'none';
-        }, 3000);
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(function(){submitError.style.display = 'none';}, 3000);
         return false;
     }
-    return true;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        if (!validateForm()) {
+            event.preventDefault();
+        }
+    });
+});
+
+
+// function validateForm() {
+//     var isNameValid = validateName();
+//     var isEmailValid = validateEmail();
+//     var isMessageValid = validateMessage();
+    
+//     if (!isNameValid || !isEmailValid || !isMessageValid) {
+//         submitError.style.display = 'block';
+//         submitError.innerHTML = 'Please fix errors to submit';
+//         setTimeout(function () {
+//             submitError.style.display = 'none';
+//         }, 3000);
+//         return false;
+//     }
+//     return true;
+// }
+
+/* scroll sections active Links */
+
+Let sections = document.querySelectorAll('section');
+Let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        Let top = window.scrollY;
+        Let offset = sec.offsetTop - 150;
+        Let height = sec.offsetHeight;
+        Let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList .add('active');
+            });
+        };
+    });
+
+// sticky navbar
+Let header = document.querySelector
+};
